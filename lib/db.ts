ed8442e93b4725db03fb0e83ce0a18ee49467ffd
ijasonbright <jason@taskforce.com.au>
2025-09-27 20:22:10 +1000

    Fix for Prisma Issue

--- lib/db.ts
+++ lib/db.ts
@@ -1,14 +1,14 @@
 import { PrismaClient } from '@prisma/client'
 
 // Prevent multiple instances of Prisma Client in development
-declare global {
-  var prisma: PrismaClient | undefined
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
 }
 
-export const prisma = global.prisma || new PrismaClient()
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
-if (process.env.NODE_ENV === 'development') {
-  global.prisma = prisma
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma
 }
 
 export default prisma
